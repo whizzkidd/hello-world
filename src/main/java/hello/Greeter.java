3eace7f1bb08b2fe873e0cd9daed70eefbb65035
whizzkidd <redon.kallaverja2@fshnstudent.info>
2018-09-22 13:55:22 +0200

    changing message

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,7 +1,7 @@
 package hello;
 
 public class Greeter {
-    public static final String message = "Hello World!!";
+    public static final String message = "Hello!!";
     public String sayHello() {
         return message;
     }
