7bf754dd413ccfdf999604082744630eee754637
whizzkidd <36606820+whizzkidd@users.noreply.github.com>
2018-09-30 16:58:15 +0200

    Updating mesage

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,7 +1,7 @@
 package hello;
 
 public class Greeter {
-    public static final String message = "Hello!!";
+    public static final String message = "Hello World!";
     public String sayHello() {
         return message;
     }
